dd2dcf278e9306069c0c03a26fc07579830c3c9d
AndrewJHalisky <ajhalisky@gmail.com>
2023-01-06 16:17:50 -0500

    Finished prep 4.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        List<int> numbers = new List<int>();
+
+        int number = -1;
+        while (number != 0)
+        {
+            Console.Write("Enter a list of numbers, type 0 when finished. ");
+
+            string response = Console.ReadLine();
+            number = int.Parse(response);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        }
+        int sum = 0;
+        foreach (int answernumber in numbers)
+        {
+            sum += answernumber;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int answernumber in numbers)
+        {
+            if (answernumber > max)
+            {
+                max = answernumber;
+            }
+
+        }
+
+        Console.WriteLine($"The max is: {max}");
+    
+    } 
 }

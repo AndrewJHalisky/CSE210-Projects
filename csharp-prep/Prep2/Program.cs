a43e1791513298d82bac3fa1d7b6ba3226fe9260
AndrewJHalisky <ajhalisky@gmail.com>
2023-01-06 13:07:54 -0500

    Finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade number: ");
+        string valueFromUser = Console.ReadLine();
+        int percent = int.Parse(valueFromUser);
+        
+        if (percent >= 90)
+        {
+            Console.WriteLine("Your graede is an A");
+        }
+        
+        else if (percent >= 80)
+        {
+            Console.WriteLine("Your graede is a B");
+        }
+
+        else if (percent >= 70)
+        {
+            Console.WriteLine("Your graede is a C");
+        }
+
+        else if (percent >= 60)
+        {
+            Console.WriteLine("Your graede is a D");
+        }
+
+        else if (percent < 60)
+        {
+            Console.WriteLine("Your graede is an F");
+        }
     }
 }
